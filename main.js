4f48d4b9420e98bf6be07bab574f1bc8ccc683c5
Dennis Muensterer <dennismuensterer@aol.com>
2021-02-20 00:08:57 +0100

    Update url

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
 // Replace this with your Sheety URL
 // Make sure NOT to include the sheet name in the URL (just the project name!)
-var projectUrl = 'https://api.sheety.co/phill/productQuest';
+var projectUrl = 'https://api.sheety.co/868a53474b5dfd812cbccdcd9f3a5736/productQuest/products';
 
 function loadProducts() {
 	fetch(projectUrl + '/products')
